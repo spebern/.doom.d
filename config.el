a82dde5800751977a5c790d0e1039fd1f6c5538b
bold <bold@cryptoguru.com>
2020-07-22 17:02:56 +0200

    Use dropbox folder for org files

--- config.el
+++ config.el
@@ -31,7 +31,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/Dropbox/org/")
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
