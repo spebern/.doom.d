0088c3013bdf8cd64ae9b6396973c2f2330e48f8
Bernhard Specht <bernhard@specht.net>
2022-10-31 16:30:29 +0100

    enable lsp for nix

--- config.el
+++ config.el
@@ -151,9 +151,3 @@
 (setq bookmark-default-file "~/OneDrive/bookmarks")
 
 (load! "+bindings.el")
-
-(add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
-(lsp-register-client
- (make-lsp-client :new-connection (lsp-stdio-connection '("rnix-lsp"))
-                  :major-modes '(nix-mode)
-                  :server-id 'nix))
