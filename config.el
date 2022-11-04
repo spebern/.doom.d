bbe7ff48c7b6352ceb8122d55018eeb2b55bf551
b.specht <bernhard.specht@pentlandfirth.com>
2022-11-04 09:40:37 +0100

    remove wl copy gruft

--- config.el
+++ config.el
@@ -90,22 +90,6 @@
 ;; org babel http
 (use-package! ob-http)
 
-;;(setq wl-copy-process nil)
-;;(defun wl-copy (text)
-;;  (setq wl-copy-process (make-process :name "wl-copy"
-;;                                      :buffer nil
-;;                                      :command '("wl-copy" "-f" "-n")
-;;                                      :connection-type 'pipe))
-;;  (process-send-string wl-copy-process text)
-;;  (process-send-eof wl-copy-process))
-
-;;(defun wl-paste ()
-;;  (if (and wl-copy-process (process-live-p wl-copy-process))
-;;      nil ; should return nil if we're the current paste owner
-;;    (shell-command-to-string "wl-paste -n")))
-;;(setq interprogram-cut-function 'wl-copy)
-;;(setq interprogram-paste-function 'wl-paste)
-
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
