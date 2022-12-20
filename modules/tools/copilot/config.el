bf0ce1a1075bb4d3e1246ead02aa61b61ec33013
Bernhard Specht <bernhard@specht.net>
2022-12-20 21:55:52 +0100

    copilot tweaks

--- modules/tools/copilot/config.el
+++ modules/tools/copilot/config.el
@@ -1,5 +1,6 @@
 ;;; tools/copilot/config.el -*- lexical-binding: t; -*-
 
+
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
   :bind (("C-TAB" . 'copilot-accept-completion-by-word)
@@ -7,5 +8,4 @@
          :map copilot-completion-map
          ("<tab>" . 'copilot-accept-completion)
          ("TAB" . 'copilot-accept-completion))
-  :config
-  (setq copilot-enable-predicates nil))
+  :config)
